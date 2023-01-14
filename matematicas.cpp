27770791605b9bb4a3672414d8ef8d433213a587
ITM Developers <itm.developers.mx@gmail.com>
2023-01-13 19:59:22 -0600

    Initial commit

---
+++ matematicas.cpp
@@ -0,0 +1,5 @@
+#include "matematicas.h"
+
+Matematicas::Matematicas()
+{
+}
---
+++ matematicas.h
@@ -0,0 +1,12 @@
+#ifndef MATEMATICAS_H
+#define MATEMATICAS_H
+
+#include "LibMatematicas_global.h"
+
+class LIBMATEMATICAS_EXPORT Matematicas
+{
+public:
+    Matematicas();
+};
+
+#endif // MATEMATICAS_H
